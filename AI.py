b856463ba9dfce01b576009771e75f2bcd8b99e9
Joshua Rinaldi <jarinaldi94@gmail.com>
2016-11-15 16:14:13 -0700

    now we're making feature vectors

---
+++ AI.py
@@ -0,0 +1,30 @@
+import tictactoe
+import random
+
+def getFeat(board):
+	markers = []
+	taken = []
+	for i in range (0, 9):
+		markers.append(board.grid[i][0])
+		taken.append(board.grid[i][1])
+	return markers, taken
+	
+def main():
+	test = tictactoe.Grid()
+	a = []
+	for i in range (0, 9):
+		a.append(i)
+	print "Test"
+	tester = tictactoe.Grid()
+	ret = 1
+	random.shuffle(a)
+	for j in a:
+		if (ret == 1):
+			ret = tester.move(j)
+	#print ret
+	markers, taken = getFeat(tester)
+	print markers
+	print taken
+	print tester.grid
+
+main()
