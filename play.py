be859e87aaeacd4a2f7f1328fbbe7e4cfeec7ac7
Wopke123 <carloslawrence58@gmail.com>
2016-11-15 09:47:42 -0700

    Added play.py to be the main file run, moved tic-tac-toe.py to tictactoe.py so we can import it

---
+++ play.py
@@ -0,0 +1,10 @@
+import tictactoe as game
+
+if __name__ == "__main__":
+    test = game.Grid()
+    test.move(0)
+    test.move(4)
+    test.move(6)
+    test.move(3)
+    test.move(1)
+    test.move(5)
