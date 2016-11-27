fdf55bfc4da794a1408af0b46f19c39c0dd2c8e2
Joshua Rinaldi <jarinaldi94@gmail.com>
2016-11-27 15:55:06 -0700

    Can't believe this actually worked

--- AI.py
+++ AI.py
@@ -1,6 +1,60 @@
 import tictactoe
 import random
 
+class ai:
+	def __init__(self):	
+		#this vector will contain the previously seen states, and the weight vector
+		self.moves = []
+
+	def chooseMove(self, board, player):
+		features = getFeat(board, player)
+		for i in range (0, len(self.moves)):
+			if features == self.moves[i][0]:
+				bestWeight = 0
+				for j in range (0,9):
+					if self.moves[i][0][j] > self.moves[i][0][bestWeight]:
+						bestWeight = j
+				return bestWeight
+		return returnRandom(board)
+
+	def train(self, hist, win):
+		if win == True:
+			for i in reversed(hist):
+				found = False
+				for j in range(0, len(self.moves)):
+					if i[0] == self.moves[j][0]:
+						self.moves[j][1][i[1]] += 1
+						found = True
+						break
+				if found == False:
+					tempWeight = []
+					for h in range(0,9):
+						if h == i[1]:
+							tempWeight.append(1)
+						else:
+							tempWeight.append(0)
+					self.moves.append([i[0], tempWeight])
+
+		elif win == False:
+			for i in hist:
+				found = False
+				for j in range(0, len(self.moves)):
+					if i[0] == self.moves[j][0]:
+						self.moves[j][1][i[1]] -= 1
+						found = True
+						break
+				if found == False:
+					tempWeight = []
+					for h in range(0,9):
+						if h == i[1]:
+							tempWeight.append(-1)
+						else:
+							tempWeight.append(0)
+					self.moves.append([i[0], tempWeight])
+
+
+
+
 def returnRandom(board):
     options = []
     for i in range (0, 9):
@@ -8,30 +62,34 @@ def returnRandom(board):
             options.append(i)
     return random.choice(options)
 
-def getFeat(board):
+def getFeat(board, player):
 	markers = []
-	taken = []
 	for i in range (0, 9):
 		markers.append(board.grid[i][0])
-		taken.append(board.grid[i][1])
-	return markers, taken
-	
-def main():
-	test = tictactoe.Grid()
-	a = []
-        print returnRandom(test)
-	for i in range (0, 9):
-		a.append(i)
-	print "Test"
-	tester = tictactoe.Grid()
-	ret = 1
-	random.shuffle(a)
-	for j in a:
-		if (ret == 1):
-			ret = tester.move(j)
-			markers, taken = getFeat(tester)
-			print markers
-			print taken
-	print ret
+	for i in range (0,len(markers)):
+		if player == "p1":
+			if markers[i] == "X":
+				markers[i] = 1
+			elif markers[i] == "O":
+				markers[i] = -1
+			else:
+				markers[i] = 0
+		elif player == "p2":
+			if markers[i] == "X":
+				markers[i] = -1
+			elif markers[i] == "O":
+				markers[i] = 1
+			else:
+				markers[i] = 0
+	return markers
+
+#def main():
+#	board = [["X", True], [" ", False], ["O", True], [" ", False], ["X", True], ["O", True], [" ", False], [" ", False], [" ", False]]
+#	p1 = "p1"
+#	p2 = "p2"
+#	p1m = getFeat(board, p1)
+#	p2m = getFeat(board, p2)
+#	print p1m
+#	print p2m
 
 #main()
--- play.py
+++ play.py
@@ -13,13 +13,38 @@ import random
 
 def playGame(p1, p2):
     newgame = game.Grid()
+    p1moves = []
+    p2moves = []
     move = [p1, "p1"]
     gameComplete = 1
     while(gameComplete == 1):
+        moveMade = 0
         gameComplete = 0
+
+        temp = (AI.getFeat(newgame, move[1]))
+
         while(gameComplete == 0):
-            gameComplete = newgame.move(makeMove(move[0], newgame))
+            moveMade = makeMove(move, newgame)
+            gameComplete = newgame.move(moveMade)
+        
+        if move[1] == "p1":
+            p1moves.append([temp, moveMade])
+        elif move[1] == "p2":
+            p2moves.append([temp, moveMade])
+
         move = switchTurn(move, p1, p2)
+    if gameComplete == "X":
+        p1[0].train(p1moves, True)
+        p2[0].train(p2moves, False)
+    elif gameComplete == "O":
+        p1[0].train(p1moves, False)
+        p2[0].train(p2moves, True)
+    else:
+        p1[0].train(p1moves, True)
+        p2[0].train(p2moves, True)
+
+    print p1[0].moves
+    print p2[0].moves
 
 #playGame helper functions 
 
@@ -27,11 +52,11 @@ def playGame(p1, p2):
 #appropriate function given the player type. (Human asks
 #for stdin, AI calls the AI, etc.)
 def makeMove(ptype, board):
-    if(ptype == 'h'):
+    if(ptype[0][1] == 'h'):
         board.show()
         return int(raw_input("Move: "))
-    elif(ptype == 'a'):
-        return AI.returnRandom(board)
+    elif(ptype[0][1] == 'a'):
+        return ptype[0][0].chooseMove(board, ptype[1])
     return 0
 
 #given the current turn, swaps to the next turn
@@ -48,21 +73,22 @@ if __name__ == "__main__":
     #Step 2: maximize reward from moves
     #Step 3: add complexity
 
-    playGame('a', 'h')
+    player1 = AI.ai()
+    player2 = AI.ai()
 
     while(raw_input("Continue? ") != "n"):
-        playGame('a', 'a')
+        playGame([player1, 'a'], [player2, 'a'])
 
-    hinput = ""
-    numarr = []
-    for i in range (0, 9):
-        numarr.append(i)
-    while(hinput != "n"):
-        print "\nNew Test:"
-        tester = game.Grid()
-        ret = 1
-        random.shuffle(numarr)
-        for j in numarr:
-            if (ret == 1):
-                ret = tester.move(j)
-        hinput = raw_input("continue? ")
+    #hinput = ""
+    #numarr = []
+    #for i in range (0, 9):
+    #    numarr.append(i)
+    #while(hinput != "n"):
+    #    print "\nNew Test:"
+    #    tester = game.Grid()
+    #    ret = 1
+    #    random.shuffle(numarr)
+    #    for j in numarr:
+    #        if (ret == 1):
+    #            ret = tester.move(j)
+    #    hinput = raw_input("continue? ")
