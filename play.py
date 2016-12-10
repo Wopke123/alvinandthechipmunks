992091f90e2ccc2827cfebc35d2948253d663709
Wopke123 <carloslawrence58@gmail.com>
2016-12-10 12:59:36 -0700

    Fixed issues to deal with stupid users, reduced the number of AI models to 1

--- play.py
+++ play.py
@@ -11,7 +11,7 @@ import random
 #
 ########################################################
 
-def playGame(p1, p2):
+def playGame(p1, p2, AImodel):
     newgame = game.Grid()
     p1moves = []
     p2moves = []
@@ -39,6 +39,10 @@ def playGame(p1, p2):
         if p2[1] == 'a':
             p2[0].train(p2moves, False)
         if (p1[1] == 'h' or p2[1] == 'h'):
+            if(p1[1] == 'h'):
+                AImodel.train(p1moves, True)
+            if(p2[1] == 'h'):
+                AImodel.train(p2moves, False)
             newgame.show()
             print "X Wins!"
         return "X"
@@ -48,6 +52,10 @@ def playGame(p1, p2):
         if p2[1] == 'a':
             p2[0].train(p2moves, True)
         if (p1[1] == 'h' or p2[1] == 'h'):
+            if(p1[1] == 'h'):
+                AImodel.train(p1moves, False)
+            if(p2[1] == 'h'):
+                AImodel.train(p2moves, True)
             newgame.show()
             print "O Wins!"
         return "O"
@@ -72,7 +80,16 @@ def playGame(p1, p2):
 def makeMove(ptype, board):
     if(ptype[0][1] == 'h'):
         board.show()
-        return int(raw_input("Move: "))
+        humove = -1
+        while(humove < 0 or humove > 8):
+            humove = raw_input("Move: ")
+            try:
+                humove = int(humove)
+                break
+            except ValueError:
+                humove = -1
+                print "Must be a number between 0 and 8"
+        return humove
     elif(ptype[0][1] == 'a'):
         return ptype[0][0].chooseMove(board, ptype[1])
     elif(ptype[0][1] == 'r'):
@@ -93,20 +110,23 @@ if __name__ == "__main__":
     #Step 2: maximize reward from moves
     #Step 3: add complexity
 
-    player1 = AI.ai()
-    p1save = 'p1save'
-    player1.loadSave(p1save)
-    player2 = AI.ai()
-    p2save = 'p2save'
-    player2.loadSave(p2save)
+    model = AI.ai()
+    modelsave = 'modelsave'
+    model.loadSave(modelsave)
     results = [0, 0, 0, 0] #X wins, O wins, Draws, num games
 
+    #while(raw_input("Continue? ") != "n"):
+
     numruns = int(raw_input("Runs: "))
- #   while(raw_input("Continue? ") != "n"):
+    while(raw_input("Continue? ") != "n"):
+        playGame([model, 'a'], ['null', 'h'], model)
+        raw_input("Press enter to continue...")
+        playGame(['null', 'h'], [model, 'a'], model)
+
     for i in range (0, numruns):
-        res = playGame([player1, 'a'], [player2, 'a'])
-        playGame([player1, 'a'], ['null', 'r'])
-        playGame(['null', 'r'], [player2, 'a'])
+        res = playGame([model, 'a'], [model, 'a'], model)
+        playGame([model, 'a'], ['null', 'r'], model)
+        playGame(['null', 'r'], [model, 'a'], model)
         results[3] += 1
         if res == "X": 
             results[0] += 1
@@ -120,8 +140,8 @@ if __name__ == "__main__":
     p1rand = []
     p2rand = []
     for i in range(0, 10):
-        p1rand.append(random.choice(player1.moves))
-        p2rand.append(random.choice(player2.moves))
+        p1rand.append(random.choice(model.moves))
+        p2rand.append(random.choice(model.moves))
     for i in p1rand:
         print i
     for i in p2rand:
@@ -130,10 +150,10 @@ if __name__ == "__main__":
     print "X:", float(results[0]) / results[3], "\tO:", float(results[1]) / results[3], "\tD:", float(results[2]) / results[3]
 
     while(raw_input("Continue (p2)? ") != "n"):
-        playGame([player1, 'a'], ['null', 'h'])
+        playGame([model, 'a'], ['null', 'h'], model)
 
     while(raw_input("Continue (p1)? ") != "n"):
-        playGame(['null', 'h'], [player2, 'a'])
+        playGame(['null', 'h'], [model, 'a'], model)
 
-    player1.saveSelf(p1save)
-    player2.saveSelf(p2save)
+    model.saveSelf(modelsave)
+    model.saveSelf(modelsave)
